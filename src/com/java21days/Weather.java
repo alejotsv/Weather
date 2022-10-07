23ec99d8d6a898d856314939babefb1bf3f262ef
Alejandro Salas <asalas.wd@outlook.com>
2022-10-07 12:10:51 -0400

    Add method to convert from Celsius to Fahrenheit

--- src/com/java21days/Weather.java
+++ src/com/java21days/Weather.java
@@ -4,11 +4,20 @@ public class Weather {
     public static void main(String[] args) {
         float fah = 86;
         System.out.println(fah + " degrees Fahrenheit is ...");
-        float cel = convertToCelsius(fah);
-        System.out.println(cel);
+        float toCel = convertToCelsius(fah);
+        System.out.println(toCel + " degrees Celsius.");
+
+        float cel = 30;
+        System.out.println((cel + "degrees Celsius is ..."));
+        float toFah = convertToFahrenheit(cel);
+        System.out.println(toFah + " degrees Fahrenheit.");
     }
 
     static float convertToCelsius(float fah) {
         return (fah - 32)/9*5;
+    }
+
+    static float convertToFahrenheit(float cel) {
+        return (cel*9/5) + 32;
     }
 }
